ee8bb553f242fd68fb5cbc005b4c4d867323ee40
SammyObeezy <barasam07@gmail.com>
2025-02-25 21:01:07 +0300

    Updated all UI elements, added the server side folder for implementatio

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -67,6 +67,33 @@ const AddCourse = () => {
       );
     }
   };
+
+  const addLecture = () => {
+    setChapters(
+      chapters.map((chapter) => {
+        if (chapter.chapterId === currentChapterId) {
+          const newLecture = {
+            ...lectureDetails,
+            lectureOrder: chapter.chapterContent.length > 0 ? chapter.chapterContent.slice(-1)[0].lectureOrder + 1 : 1,
+            lectureId: uniqid(),
+          };
+          chapter.chapterContent.push(newLecture);
+        }
+        return chapter;
+      })
+    );
+    setShowPopup(false);
+    setLectureDetails({
+      lectureTitle: '',
+      lectureDuration: '',
+      lectureUrl: '',
+      isPreviewFree: false,
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   useEffect(() => {
     //Intiate quil once
     if (!quillRef.current && editorRef.current) {
@@ -78,7 +105,7 @@ const AddCourse = () => {
   return (
     <div className='h-screen overflow-scroll flex flex-col items-start
     justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
-      <form className='flex flex-col gap-4 max-w-md w-full text-gray-500'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 max-w-md w-full text-gray-500'>
         <div className='flex flex-col gap-1'>
           <p>Course Title</p>
           <input onChange={e => setCourseTitle(e.target.value)} value={courseTitle} type="text" placeholder='Type here' className='outline-none md:py-2.5
@@ -115,12 +142,12 @@ const AddCourse = () => {
               <div key={chapterIndex} className='bg-white border rounded-lg mb-4'>
                 <div className='flex justify-between items-center p-4 border-b'>
                   <div className='flex items-center'>
-                    <img src={assets.dropdown_icon} width={14} alt="" className={`
+                    <img onClick={() => handleChapter('toggle', chapter.chapterId)} src={assets.dropdown_icon} width={14} alt="" className={`
                       mr-2 cursor-pointer transition-all ${chapter.collapsed && "-rotate-90"}`} />
                     <span className="font-semibold">{chapterIndex + 1} {chapter.chapterTitle}</span>
                   </div>
                   <span className='text-gray-500'>{chapter.chapterContent.length} Lectures</span>
-                  <img src={assets.cross_icon} alt="" className='cursor-pointer' />
+                  <img onClick={() => handleChapter('remove', chapter.chapterId)} src={assets.cross_icon} alt="" className='cursor-pointer' />
                 </div>
                 {!chapter.collapsed && (
                   <div className='p-4'>
@@ -142,7 +169,7 @@ const AddCourse = () => {
           {
             showPopup && (
               <div className='fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50'>
-                <div className='bg-white text-gray-700 p-4 rounded relative w-full max-w-8'>
+                <div className='bg-white text-gray-700 p-4 rounded relative w-full max-w-80'>
                   <h2 className='text-lg font-semibold mb-4'>Add Lecture</h2>
                   <div className="mb-2">
                     <p>Lecture Title</p>
@@ -174,17 +201,17 @@ const AddCourse = () => {
                         lectureUrl: e.target.value
                       })} />
                   </div>
-                  <div className="mb-2">
+                  <div className="flex gap-2 my-4">
                     <p>Is Preview Free?</p>
                     <input type="checkbox"
-                      className='mt-1 block w-full border rounded py-1 px-2'
+                      className='mt-1 scale-125'
                       value={lectureDetails.isPreviewFree}
                       onChange={(e) => setLectureDetails({
                         ...lectureDetails,
                         isPreviewFree: e.target.value
                       })} />
                   </div>
-                  <button type='button' className='w-full bg-blue-400 text-white px-4 py-2 rounded'>Add</button>
+                  <button type='button' className='w-full bg-blue-400 text-white px-4 py-2 rounded' onClick={addLecture} >Add</button>
                   <img onClick={() => setShowPopup(false)} src={assets.cross_icon}
                     className='absolute top-4 right-4 w-4 cursor-pointer' alt="" />
                 </div>
