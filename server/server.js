cdb56a19bdca6c58248f8acc6ad661624bd174ca
SammyObeezy <barasam07@gmail.com>
2025-03-06 12:38:34 +0300

    Server.js updated

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 // Intilize Express
 
@@ -10,12 +12,18 @@ const app = express()
 
 // Connect to DB
 await connectDB()
+
+
 //Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
+
+
 
 //Routes
 app.get('/', (req, res) => res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
+app.use('/educator', express.json(), educatorRouter)
 //Port 
 
 const PORT = process.env.PORT || 5000
