971c29b4094b7c1580c1ac0dfb5a06a43dbf09ce
SammyObeezy <barasam07@gmail.com>
2025-04-19 10:53:53 +0300

    Fixed errors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
+import bodyParser from 'body-parser'
 
 // Intialize Express
 
@@ -22,7 +23,8 @@ app.use(clerkMiddleware())
 
 //Routes
 app.get('/', (req, res) => res.send("API Working"))
-app.post('/clerk', express.json(), clerkWebhooks)
+// app.post('/clerk', express.json(), clerkWebhooks)
+app.post('/clerk', bodyParser.raw({ type: 'application/json' }), clerkWebhooks)
 app.use('/educator', express.json(), educatorRouter)
 //Port 
 
